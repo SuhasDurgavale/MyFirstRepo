7dac73bfa31636bed9fc65d4fabfac908a8594bb
suhas2602 <suhasdurgavale2602@gmail.com>
2024-03-03 15:45:58 +0530

    [suhas2602] added snapdeal class by me

---
+++ src/test/java/testLayer/Snapdeal.java
@@ -0,0 +1,21 @@
+package testLayer;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Snapdeal {
+	
+		 @Test
+		  public void OpenAmazon() 
+		  {
+			  WebDriver driver=new ChromeDriver();
+			  driver.get("https://www.snapdeal.com/");
+			  driver.manage().window().maximize();
+			  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			  
+			  
+			  }
+}
